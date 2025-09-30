5ec23e37f31c0b005fe2c383597f2cb7d901f131
Jhon Henao <j.henao@cgiar.org>
2025-09-30 08:47:59 -0500

    Feat: Update home page

--- campusmap-client/src/app/pages/home/home.ts
+++ campusmap-client/src/app/pages/home/home.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { Button } from "primeng/button";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, Button],
   templateUrl: './home.html'
 })
 export class Home implements OnInit {
@@ -19,6 +20,6 @@ export class Home implements OnInit {
   setLanguage(lang: 'es' | 'en') {
     this.language = lang;
     localStorage.setItem('lang', lang);
-    console.log('Idioma seleccionado:', lang);
+    console.log('Lang:', lang);
   }
 }
