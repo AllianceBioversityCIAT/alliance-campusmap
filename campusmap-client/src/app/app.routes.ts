f30c6a389205f543c3a9bab892e52f4e8a9f4340
Juan Pablo Bustamante <74508476+juanpbustamante@users.noreply.github.com>
2025-09-30 10:26:07 -0500

    i18n implementation

--- campusmap-client/src/app/app.routes.ts
+++ campusmap-client/src/app/app.routes.ts
@@ -1,8 +1,8 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-  { path: '', loadComponent: () => import('./pages/welcome/welcome').then(m => m.Welcome) },
-  { path: 'welcome', loadComponent: () => import('./pages/welcome/welcome').then(m => m.Welcome) },
-  { path: 'home', loadComponent: () => import('./pages/home/home').then(m => m.Home) },
-  { path: 'map', loadComponent: () => import('./pages/map/map').then(m => m.Map) }
+  { path: '', loadComponent: () => import('./modules/auth/pages/welcome/welcome').then(m => m.Welcome) },
+  { path: 'welcome', loadComponent: () => import('./modules/auth/pages/welcome/welcome').then(m => m.Welcome) },
+  { path: 'home', loadComponent: () => import('./modules/auth/pages/home/home').then(m => m.Home) },
+  { path: 'map', loadComponent: () => import('./modules/map/pages/map/map').then(m => m.Map) }
 ];
--- campusmap-client/src/app/modules/auth/pages/home/home.ts
+++ campusmap-client/src/app/modules/auth/pages/home/home.ts
@@ -1,25 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
-import { Button } from "primeng/button";
+import { RouterLink } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, RouterModule, Button],
-  templateUrl: './home.html'
+  imports: [CommonModule, RouterLink, ButtonModule, TranslateModule],
+  templateUrl: './home.html',
 })
-export class Home implements OnInit {
-  language: 'es' | 'en' = 'es';
+export class Home {
+  language: 'en' | 'es' = 'en';
 
-  ngOnInit(): void {
-    const savedLang = localStorage.getItem('lang') as 'es' | 'en';
-    this.language = savedLang || 'es';
+  constructor(private translate: TranslateService) {
+    const savedLang = (localStorage.getItem('lang') as 'en' | 'es') || 'en';
+    this.language = savedLang;
+    this.translate.addLangs(['en', 'es']);
+    this.translate.setDefaultLang('en');
+    this.translate.use(savedLang);
   }
 
-  setLanguage(lang: 'es' | 'en') {
+  setLanguage(lang: 'en' | 'es') {
     this.language = lang;
+    this.translate.use(lang);
     localStorage.setItem('lang', lang);
-    console.log('Lang:', lang);
   }
 }
