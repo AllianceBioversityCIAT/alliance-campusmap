95d0ab1fdc156f45eb872e5ab5695450abedcd9e
Juan Pablo Bustamante <jpbustamanteb@correo.usbcali.edu.co>
2025-10-08 11:58:02 -0500

    Add unit tests for Home and Welcome components
    Introduces basic unit tests for the Home and Welcome components to verify their creation. Also increases the initial wait time in Welcome component from 3000ms to 5000ms before navigation.

---
+++ campusmap-client/src/app/modules/auth/pages/welcome/welcome.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { Welcome } from './welcome';
+
+describe('Map', () => {
+  let component: Welcome;
+  let fixture: ComponentFixture<Welcome>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [Map]
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(Welcome);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
