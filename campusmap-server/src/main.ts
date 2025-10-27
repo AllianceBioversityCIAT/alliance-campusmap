3d3a7d8eaa929a66740be235b7433bc6c32ec5c2
cserratoa <c.serrato@cgiar.org>
2025-10-27 09:41:16 -0500

    feat(places): add endpoints to get all places and places by type

--- campusmap-server/src/main.ts
+++ campusmap-server/src/main.ts
@@ -13,8 +13,8 @@ async function bootstrap() {
   const adapterHost = app.get(HttpAdapterHost);
   app.useGlobalFilters(new AllExceptionsFilter(adapterHost, appLogger));
 
-   app.setGlobalPrefix('api/v1');
-   
+  app.setGlobalPrefix('api/v1');
+  app.enableCors();
   const config = new DocumentBuilder()
     .setTitle('CampusMap Server')
     .setDescription('API de CampusMap')
