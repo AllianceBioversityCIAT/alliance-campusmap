a2e9da615b582c8e86e0881c19e58ed27d47beab
jpbustamanteb99 <juanbermudez026@gmail.com>
2025-11-06 09:36:21 -0500

    Add InformationPopUpUserSelection component
    Introduces the InformationPopUpUserSelection Angular component with its template, style, and basic unit test. This sets up the structure for a new user selection pop-up feature in the map module.

---
+++ campusmap-client/src/app/modules/map/pages/map/components/information-pop-up-user-selection/information-pop-up-user-selection.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-information-pop-up-user-selection',
+  imports: [],
+  templateUrl: './information-pop-up-user-selection.html',
+  styleUrl: './information-pop-up-user-selection.scss'
+})
+export class InformationPopUpUserSelection {
+
+}
