bfb6518a3a4fa11b71fe70f6b922bf9e97a6e3fc
jpbustamanteb99 <juanbermudez026@gmail.com>
2025-11-06 09:08:10 -0500

    Add InformationPopUpParking component to map page
    Introduces the InformationPopUpParking Angular component with its template, style, and test files. Integrates the new component into the map page by updating the HTML and TypeScript to include it in the imports and template.

---
+++ campusmap-client/src/app/modules/map/pages/map/components/information-pop-up-parking/information-pop-up-parking.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-information-pop-up-parking',
+  imports: [],
+  templateUrl: './information-pop-up-parking.html',
+  styleUrl: './information-pop-up-parking.scss'
+})
+export class InformationPopUpParking {
+
+}
--- campusmap-client/src/app/modules/map/pages/map/map.ts
+++ campusmap-client/src/app/modules/map/pages/map/map.ts
@@ -3,11 +3,12 @@ import { MapLoad } from './components/map-load/map-load';
 import { SearchBar } from './components/search-bar/search-bar';
 import { FilterControls } from './components/filter-controls/filter-controls';
 import { InformationPopUp } from './components/information-pop-up/information-pop-up';
+import { InformationPopUpParking } from './components/information-pop-up-parking/information-pop-up-parking';
 
 @Component({
   selector: 'app-map',
   standalone: true,
-  imports: [MapLoad, SearchBar, FilterControls, InformationPopUp],
+  imports: [MapLoad, SearchBar, FilterControls, InformationPopUp, InformationPopUpParking],
   templateUrl: './map.html',
   styleUrls: ['./map.scss']
 })
