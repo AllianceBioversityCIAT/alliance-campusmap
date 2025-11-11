df4800ff5e926683d38788391adae3ad019a0b2a
jpbustamanteb99 <juanbermudez026@gmail.com>
2025-11-11 09:37:45 -0500

    Update map.ts

--- campusmap-client/src/app/modules/map/pages/map/map.ts
+++ campusmap-client/src/app/modules/map/pages/map/map.ts
@@ -2,12 +2,11 @@ import { Component } from '@angular/core';
 import { MapLoad } from './components/map-load/map-load';
 import { SearchBar } from './components/search-bar/search-bar';
 import { FilterControls } from './components/filter-controls/filter-controls';
-import { InformationPopUpTimer } from './components/information-pop-up-timer/information-pop-up-timer';
 
 @Component({
   selector: 'app-map',
   standalone: true,
-  imports: [MapLoad, SearchBar, FilterControls, InformationPopUpTimer],
+  imports: [MapLoad, SearchBar, FilterControls],
   templateUrl: './map.html',
   styleUrls: ['./map.scss']
 })
